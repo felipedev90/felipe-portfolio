a9a49874508c98eaab00aea9b1560aa599c2c234
Felipe Augusto <augusto.felipedev@gmail.com>
2026-03-05 15:03:56 -0300

    fix: rename components to .tsx and add CSS Modules type declaration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Navbar from "./components/Navbar/NavBar.jsx";
-import Hero from "./components/Hero/Hero.jsx";
+import Navbar from "./components/NavBar/NavBar.tsx";
+import Hero from "./components/Hero/Hero.tsx";
+import About from "./components/About/About.tsx";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
